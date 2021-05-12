c6f7f182440180c42610bcd31900586a3f2a0c92
Filip <filip@golem.network>
2021-05-12 16:13:24 +0200

    Add multiactivity integration test

---
+++ tests/goth/test_multiactivity_agreement/requestor.ts
@@ -0,0 +1,41 @@
+import { Executor, Task, utils, vm, WorkContext } from "../../../yajsapi";
+
+const { asyncWith, logUtils, range } = utils;
+
+async function main() {
+  const _package = await vm.repo({
+    image_hash: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
+    min_mem_gib: 0.5,
+    min_storage_gib: 2.0,
+  });
+
+  async function* worker(work_ctx: WorkContext, tasks) {
+    for await (let task of tasks) {
+      work_ctx.run("/bin/sleep", ["1"]);
+      yield work_ctx.commit({ timeout: 15 * 60 * 1000 });
+      task.accept_result();
+      return;
+    }
+  }
+
+  await asyncWith(
+    new Executor({
+      task_package: _package,
+      max_workers: 1,
+      timeout: 6 * 60 * 1000,
+      budget: "10.0",
+      subnet_tag: "devnet-beta.1",
+      event_consumer: logUtils.logSummary(),
+    }),
+    async (executor: Executor): Promise<void> => {
+      const tasks: any[] = range(0, 3).map((frame) => new Task(frame));
+      for await (let task of executor.submit(worker, tasks)) {
+        console.log(`Task computed: ${task.result()}`);
+      }
+    }
+  );
+  return;
+}
+
+utils.changeLogLevel("debug");
+main();
---
+++ tests/goth/test_multiactivity_agreement/test_multiactivity_agreement.py
@@ -0,0 +1,85 @@
+"""A goth test scenario for multi-activity agreements."""
+from functools import partial
+import logging
+import os
+from pathlib import Path
+import re
+
+import pytest
+
+import goth.configuration
+from goth.runner import Runner
+from goth.runner.probe import RequestorProbe
+from goth.runner.log import configure_logging
+
+
+logger = logging.getLogger("goth.test.multiactivity_agreement")
+
+
+async def assert_agreement_created(events):
+    """Assert that `AgreementCreated` event occurs."""
+
+    async for line in events:
+        m = re.match(r"New agreement. id: ([^,]+)", line)
+        if m:
+            return m.group(1)
+    raise AssertionError("Expected AgreementCreated event")
+
+
+async def assert_multiple_workers_run(agr_id, events):
+    """Assert that more than one worker is run with given `agr_id`.
+    Fails if a worker failure is detected or if a worker has run for another agreement.
+    """
+    workers_finished = 0
+
+    async for line in events:
+        m = re.match(r"Stopped worker related to agreement ([0-9a-f]+)", line)
+        if m:
+            worker_agr_id = m.group(1)
+            assert worker_agr_id == agr_id, "Worker run for another agreement"
+            assert " finished with error" not in line, "Worker finished with error"
+            workers_finished += 1
+        elif re.match("Computation finished", line):
+            break
+
+    assert workers_finished > 1, (
+        f"Only {workers_finished} worker(s) run for agreement {agr_id}, " "expected more than one"
+    )
+
+
+@pytest.mark.asyncio
+async def test_multiactivity_agreement(project_dir: Path, log_dir: Path, config_overrides) -> None:
+
+    configure_logging(log_dir)
+
+    # Override the default test configuration to create only one provider node
+    nodes = [
+        {"name": "requestor", "type": "Requestor"},
+        {"name": "provider-1", "type": "VM-Wasm-Provider", "use-proxy": True},
+    ]
+    config_overrides.append(("nodes", nodes))
+    goth_config = goth.configuration.load_yaml(
+        project_dir / "tests" / "goth" / "assets" / "goth-config.yml",
+        config_overrides,
+    )
+
+    runner = Runner(base_log_dir=log_dir, compose_config=goth_config.compose_config)
+
+    async with runner(goth_config.containers):
+
+        requestor = runner.get_probes(probe_type=RequestorProbe)[0]
+
+        async with requestor.run_command_on_host(
+            "npm run-script test-multiactivity-agreement", env=os.environ
+        ) as (_cmd_task, cmd_monitor):
+
+            # Wait for agreement
+            assertion = cmd_monitor.add_assertion(assert_agreement_created)
+            agr_id = await assertion.wait_for_result(timeout=30)
+
+            # Wait for multiple workers run for the agreement
+            assertion = cmd_monitor.add_assertion(
+                partial(assert_multiple_workers_run, agr_id),
+                name=f"assert_multiple_workers_run({agr_id})",
+            )
+            await assertion.wait_for_result(timeout=60)
