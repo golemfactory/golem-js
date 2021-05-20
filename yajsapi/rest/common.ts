09c7a601501c80c727927dbd18c57509874b2490
filipgolem <44880692+filipgolem@users.noreply.github.com>
2021-05-20 19:32:04 +0200

    Change quotes to backticks and set interval between repeated calls to 1s (#195)
    * Change quotes to backticks
    * Change default interval to 1000ms

--- yajsapi/rest/common.ts
+++ yajsapi/rest/common.ts
@@ -28,7 +28,7 @@ export async function repeat_on_error(
   block: Callable<void, any>,
   max_tries: number = 5,
   max_duration_ms = 15000,
-  interval: number = 0.0,
+  interval: number = 1000,
   condition: Callable<Error, boolean> = is_intermittent_error
 ) {
   let start_time = Date.now();
@@ -49,7 +49,7 @@ export async function repeat_on_error(
     const duration = Date.now() - start_time;
     const repeat = try_num < max_tries && duration < max_duration_ms;
     const msg = `API call timed out (attempt ${try_num}/${max_tries}), ` +
-              (repeat ? `retrying in ${interval}s` : "giving up after ${duration}ms");
+              (repeat ? `retrying in ${interval}s` : `giving up after ${duration}ms`);
     logger.debug(msg);
     if (!repeat) { throw err_in_block; }
   }
