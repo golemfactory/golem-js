9a1c3bae763fa94bdcd672c37524d023153a61d9
filipgolem <44880692+filipgolem@users.noreply.github.com>
2021-07-02 12:35:00 +0200

    Reduce log spam and min cpu threads to b0.4 (#233)
    * Support for filtering provider offers by golem.inf.cpu.threads and golem.inf.cpu.cores (#231)
    * Send valid termination reason (#229)
    * Reduce log entries related to confirmed proposals (#230)

--- yajsapi/package/vm.ts
+++ yajsapi/package/vm.ts
@@ -5,6 +5,7 @@ class _VmConstrains extends Constraints {
   constructor(
     min_mem_gib: number,
     min_storage_gib: number,
+    min_cpu_threads: number = 1,
     cores: number = 1
   ) {
     super();
@@ -13,6 +14,7 @@ class _VmConstrains extends Constraints {
       `(${InfVmKeys["mem"]}>=${min_mem_gib})`,
       `(${InfVmKeys["storage"]}>=${min_storage_gib})`,
       `(${InfVmKeys["runtime"]}=${RuntimeType.VM})`,
+      `(${InfVmKeys["threads"]}>=${min_cpu_threads})`,
     ]);
   }
 }
@@ -21,6 +23,7 @@ export async function repo({
   image_hash,
   min_mem_gib = 0.5,
   min_storage_gib = 2.0,
+  min_cpu_threads = 1,
 }: RepoOpts): Promise<Package> {
   /*
     Builds reference to a demand decorator.
@@ -28,12 +31,13 @@ export async function repo({
     - *image_hash*: finds package by its contents hash.
     - *min_mem_gib*: minimal memory required to execute application code.
     - *min_storage_gib* minimal disk storage to execute tasks.
+    - *min_cpu_threads*: minimal available logical CPU cores (CPU threads).
 
     */
 
   return new VmPackage({
     repo_url: await resolve_repo_srv({repo_srv: DEFAULT_REPO_SRV}),
     image_hash,
-    constraints: new _VmConstrains(min_mem_gib, min_storage_gib),
+    constraints: new _VmConstrains(min_mem_gib, min_storage_gib, min_cpu_threads),
   });
 }
