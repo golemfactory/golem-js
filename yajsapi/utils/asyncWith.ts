1835e2055e91bb69f24f9ca6ab90aea2b6aa00dd
filipgolem <44880692+filipgolem@users.noreply.github.com>
2021-08-05 12:44:47 +0200

    Use event consumer cancellation token to properly stop event consumer (#248)
    * Use event consumer cancellation token to properly stop event consumer
    * Bump dependencies
    * Improve log message
    * Report payment status and total paid in logs
    * Fix error handling

--- yajsapi/utils/asyncWith.ts
+++ yajsapi/utils/asyncWith.ts
@@ -1,11 +1,9 @@
-import { logger } from "./";
 export default async function asyncWith(expression, block) {
   let mgr = expression ? await expression.ready.call(expression) : null;
   let errInBlock;
   try {
     await block(mgr);
   } catch (error) {
-    const { message, stack } = error;
     errInBlock = error;
   }
   await expression.done.call(expression, mgr);
