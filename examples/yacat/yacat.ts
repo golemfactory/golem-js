b879a63bbacdc9eca9ee630d7f011d37c2c3b112
Marcin Gordel <marcin.gordel@gmail.com>
2022-03-23 10:10:48 +0100

    Fixed RepoOpts and args rename bug (#287)

--- examples/yacat/yacat.ts
+++ examples/yacat/yacat.ts
@@ -98,8 +98,8 @@ async function main(args) {
       max_workers: args.numberOfProviders,
       budget: "10.0",
       subnet_tag: args.subnetTag,
-      driver: args.driver,
-      network: args.network,
+      payment_driver: args.driver,
+      payment_network: args.network,
     }),
     async (executor: Executor): Promise<void> => {
       let keyspace_computed = false;
