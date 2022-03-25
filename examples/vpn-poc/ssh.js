942f5e5a7a21403f626829c4549e2f98941238d2
marcin <marcin.gordel@gmail.com>
2022-03-25 20:36:48 +0100

    Added goth test for ssh (draft) (#287)

--- examples/vpn-poc/ssh.js
+++ examples/vpn-poc/ssh.js
@@ -1,27 +1,28 @@
-const { Executor, Task, utils: { asyncWith, sleep }, vm } = require("../../dist");
+const { Executor, Task, utils, vm } = require("../../dist");
+const { asyncWith, sleep } = utils;
 const crypto = require("crypto");
+const { program } = require("commander");
 
-async function main() {
-  const SESSION_TIMEOUT = 5 * 60; // 5 min
-  const SESSION_COUNT = 3;
-  const package = await vm.repo({
+async function main(subnetTag, driver, network, count = 2, session_timeout = 1) {
+  const SESSION_TIMEOUT = session_timeout * 20;
+  const _package = await vm.repo({
     image_hash: "1e06505997e8bd1b9e1a00bd10d255fc6a390905e4d6840a22a79902",
     capabilities: ['vpn']
   });
-  const tasks = new Array(SESSION_COUNT).fill(new Task(null));
+  const tasks = new Array(+count).fill(new Task(null));
 
   async function* worker(context, tasks) {
-    const password = crypto.randomBytes(3).toString('hex');
-    const connection_uri = context.network_node.get_websocket_uri(22);
-    const app_key = process.env["YAGNA_APPKEY"];
-
-    console.log("\n------------------------------------------");
-    console.log(`Connect via ssh to provider "${context.provider_info.provider_name}"`);
-    console.log(`Connect with:\nssh -o ProxyCommand='websocat asyncstdio: ${connection_uri} --binary -H=Authorization:\"Bearer ${app_key}\"' root@${crypto.randomBytes(10).toString('hex')}`);
-    console.log(`Password: ${password}`);
-    console.log("------------------------------------------\n");
-
     for await (let task of tasks) {
+      const password = crypto.randomBytes(3).toString('hex');
+      const connection_uri = context.network_node.get_websocket_uri(22);
+      const app_key = process.env["YAGNA_APPKEY"];
+
+      console.log("\n------------------------------------------");
+      console.log(`Connect via ssh to provider "${context.provider_info.provider_name}"`);
+      console.log(`Connect with:\nssh -o ProxyCommand='websocat asyncstdio: ${connection_uri} --binary -H=Authorization:\"Bearer ${app_key}\"' root@${crypto.randomBytes(10).toString('hex')}`);
+      console.log(`Password: ${password}`);
+      console.log("------------------------------------------\n");
+
       context.run("/bin/bash", ["-c", "syslogd"]);
       context.run("/bin/bash", ["-c", "ssh-keygen -A"]);
       context.run("/bin/bash", ["-c", `echo -e "${password}\n${password}" | passwd`]);
@@ -30,7 +31,11 @@ async function main() {
       const { results } = await future_result;
       if (results[results.length - 1].success) {
         await sleep(SESSION_TIMEOUT);
-        task.accept_result();
+        try {
+          task.accept_result('ok');
+        } catch (e ){
+          console.log(e);
+        }
       } else {
         task.reject_result();
       }
@@ -39,11 +44,13 @@ async function main() {
 
   await asyncWith(
     new Executor({
-      task_package: package,
+      task_package: _package,
       budget: "1.0",
-      subnet_tag: "devnet-beta",
+      subnet_tag: subnetTag,
+      driver: driver,
+      network: network,
       network_address: "192.168.0.0/24",
-      max_workers: SESSION_COUNT
+      max_workers: count
     }),
     async (executor) => {
       for await (let completed of executor.submit(worker, tasks)) {
@@ -53,4 +60,15 @@ async function main() {
   );
 }
 
-main();
+program
+  .option("--subnet-tag <subnet>", "set subnet name, for example 'devnet-beta'")
+  .option("--payment-driver, --driver <driver>", "payment driver name, for example 'erc20'")
+  .option("--payment-network, --network <network>", "network name, for example 'rinkeby'")
+  .option("--task-count, --count <count>", "task count", val => parseInt(val))
+  .option("--session-timeout, --timeout <timeout>", "ssh session timeout (in minutes)", val => parseInt(val))
+  .option("-d, --debug", "output extra debugging");
+program.parse(process.argv);
+if (program.debug) {
+  utils.changeLogLevel("debug");
+}
+main(program.subnetTag, program.driver, program.network, program.count, program.timeout);
