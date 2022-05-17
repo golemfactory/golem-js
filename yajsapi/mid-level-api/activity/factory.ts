dcf448cbe34b76f98b23f3966628b67465d781b8
Marcin Gordel <marcin.gordel@gmail.com>
2022-05-17 16:54:23 +0200

    Added rewiremock (#292)

--- yajsapi/mid-level-api/activity/factory.ts
+++ yajsapi/mid-level-api/activity/factory.ts
@@ -1,16 +1,12 @@
-import { Activity, ActivityOptions } from "./activity";
-import { RequestorControlApi, RequestorStateApi } from "ya-ts-client/dist/ya-activity/api";
-import { logger } from "../../utils";
+import { RequestorControlApi } from "ya-ts-client/dist/ya-activity/api";
 
 export class ActivityFactory {
   private api: RequestorControlApi;
   constructor() {
     this.api = new RequestorControlApi();
   }
-  async create(agreementId: string, options?: ActivityOptions): Promise<Activity> {
+  async create(agreementId: string): Promise<string> {
     const { data } = await this.api.createActivity({ agreementId });
-    const activityId = typeof data == "string" ? data : data.activityId;
-    logger.debug(`Created activity ${activityId} for agreement ${agreementId}`);
-    return new Activity(activityId, options);
+    return typeof data == "string" ? data : data.activityId;
   }
 }
