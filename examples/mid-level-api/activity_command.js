6e16671bce8987134a549514d8f8b88e657557fc
Marcin Gordel <marcin.gordel@gmail.com>
2022-06-13 11:49:54 +0200

    Cleanup activity (#292)

--- examples/mid-level-api/activity_command.js
+++ examples/mid-level-api/activity_command.js
@@ -1,20 +1,21 @@
-const { Activity, ActivityFactory } = require("../../dist/mid-level-api/activity");
-const { Run, Start, Deploy, Terminate, Command } = require("../../dist/mid-level-api/script");
+const { ActivityFactory } = require("../../dist/mid-level-api/activity");
+const { Run, Start, Deploy, Terminate } = require("../../dist/mid-level-api/script");
 
 async function main() {
   const agreementId = "769d892c72f10ca4e3df3afd96178d90edd6f5acca74bfd0a52f4aecbcecb707";
   const activityFactory = new ActivityFactory();
-  const activityId = await activityFactory.create(agreementId).catch((e) => console.error(e));
-  const activity = new Activity(activityId);
-  activity.on("StateChanged", (e) => console.log("EVENT: ", e));
+  const activity = await activityFactory.create(agreementId);
 
-  await activity.executeCommand(new Deploy());
-  await activity.executeCommand(new Start());
-  const command = new Run("/bin/sh", ["-c", "echo 'Hello World'"]);
-  const commandResult = await activity.executeCommand(command).catch((e) => console.error(e));
-  console.log("RESULTS: ", commandResult);
-  await activity.executeCommand(new Terminate());
+  await activity.execute([new Deploy().toJson()]);
+  await activity.execute([new Start().toJson()]);
+  const commandRun = new Run("/bin/sh", ["-c", "echo 'Hello World'"]);
+  const commandRunResult = await activity.execute([commandRun.toJson()]);
+  console.log("RESULTS: ", commandRunResult.read());
+  await activity.execute([new Terminate().toJson()]);
   await activity.stop();
 }
 
-main();
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
