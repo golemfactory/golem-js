82e893c6b250dc03155cdb4d9d491a0424b1fee2
Marcin Gordel <marcin.gordel@gmail.com>
2022-06-20 13:53:20 +0200

    Added examples nodejs and web for new activity module (#304)

---
+++ examples/mid-level-api/activity_command.js
@@ -0,0 +1,21 @@
+const { ActivityFactory } = require("../../dist/mid-level-api/activity");
+const { Run, Start, Deploy, Terminate } = require("../../dist/mid-level-api/script");
+
+async function main() {
+  const agreementId = "769d892c72f10ca4e3df3afd96178d90edd6f5acca74bfd0a52f4aecbcecb707";
+  const activityFactory = new ActivityFactory();
+  const activity = await activityFactory.create(agreementId);
+
+  await activity.execute(new Deploy().toExeScriptRequest());
+  await activity.execute(new Start().toExeScriptRequest());
+  const commandRun = new Run("/bin/sh", ["-c", "echo 'Hello World'"]);
+  const commandRunResult = await activity.execute(commandRun.toExeScriptRequest());
+  console.log("RESULTS: ", commandRunResult.read());
+  await activity.execute(new Terminate().toExeScriptRequest());
+  await activity.stop();
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
