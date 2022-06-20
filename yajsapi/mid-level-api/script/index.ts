aaf4096da83f323101d701f0f6bed5bd0d6f2bcf
Marcin Gordel <marcin.gordel@gmail.com>
2022-06-20 13:43:21 +0200

    cleanup (#292)

--- yajsapi/mid-level-api/script/index.ts
+++
@@ -1,3 +0,0 @@
-import { Script } from "./script";
-import { Command, Run, Deploy, Start, Transfer, Terminate, SendFile, DownloadFile } from "./command";
-export { Script, Command, Run, Deploy, Start, Transfer, Terminate, SendFile, DownloadFile };
--- yajsapi/mid-level-api/storage/provider.ts
+++
@@ -1,6 +0,0 @@
-interface StorageProvider {
-  init(): Promise<void>;
-  end(): Promise<void>;
-  download(path: string): Promise<string>;
-  upload(path: string): Promise<string>;
-}
