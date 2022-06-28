606c742bb53843c76af5a06ade4c7d3460d2ad84
Marcin Gordel <marcin.gordel@gmail.com>
2022-06-28 10:26:08 +0200

    Added unit activity streaming batch and unit tests (#316)

--- tests/mock/requestor_control_api.ts
+++ tests/mock/requestor_control_api.ts
@@ -1,13 +1,20 @@
 /* eslint @typescript-eslint/ban-ts-comment: 0 */
 import { RequestorControlApi } from "ya-ts-client/dist/ya-activity/src/api/requestor-control-api";
-import { ExeScriptCommandResult, ExeScriptRequest } from "ya-ts-client/dist/ya-activity/src/models";
-import { AxiosRequestConfig, AxiosResponse } from "axios";
+import {
+  CreateActivityRequest,
+  CreateActivityResult,
+  ExeScriptCommandResult,
+  ExeScriptRequest,
+} from "ya-ts-client/dist/ya-activity/src/models";
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { ExeScriptCommandResultResultEnum } from "ya-ts-client/dist/ya-activity/src/models/exe-script-command-result";
 
 export class RequestorControlApiMock extends RequestorControlApi {
   private expectedResults: ExeScriptCommandResult[] = [];
   private mockedResults: ExeScriptCommandResult[] = [];
+  private expectedErrors: { message: string; status: number }[] = [];
+  private mockedErrors: { message: string; status: number }[] = [];
   private exampleResult = {
     index: 0,
     eventDate: new Date().toISOString(),
@@ -28,6 +35,17 @@ export class RequestorControlApiMock extends RequestorControlApi {
       this.expectedResults[i][result[0]] = result[1];
     });
   }
+  setExpectedErrors(errors) {
+    this.expectedErrors = errors;
+  }
+  // @ts-ignore
+  async createActivity(
+    stringCreateActivityRequest: string | CreateActivityRequest,
+    timeout?: number,
+    options?: AxiosRequestConfig
+  ): Promise<import("axios").AxiosResponse<string | CreateActivityResult>> {
+    return new Promise((res) => res({ data: { activityId: uuidv4() } } as AxiosResponse));
+  }
   // @ts-ignore
   async exec(
     activityId: string,
@@ -44,7 +62,15 @@ export class RequestorControlApiMock extends RequestorControlApi {
     timeout?: number,
     options?: AxiosRequestConfig
   ): Promise<AxiosResponse<ExeScriptCommandResult[]>> {
-    // console.log(this.expectedResults);
+    if (this.expectedErrors.length) {
+      const mockError = this.expectedErrors.shift();
+      const error = new Error(mockError!.message) as AxiosError;
+      error.response = {
+        data: { message: mockError!.message },
+        status: mockError!.status,
+      } as AxiosResponse;
+      throw error;
+    }
     if (this.expectedResults?.length) {
       this.mockedResults.push(this.expectedResults?.shift() as ExeScriptCommandResult);
     }
