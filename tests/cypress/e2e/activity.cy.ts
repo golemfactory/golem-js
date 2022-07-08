f3f4ac48f2df3b367432bc6e5a7dc7cec053f695
Marcin Gordel <marcin.gordel@gmail.com>
2022-07-08 13:57:34 +0200

    Added next cypress tests (#326)

--- tests/cypress/e2e/activity.cy.ts
+++ tests/cypress/e2e/activity.cy.ts
@@ -45,4 +45,21 @@ describe("Test standalone activity module", () => {
     cy.contains("Destroy Activity").click();
     cy.get("#logs").should("include.text", "Activity has been destroyed");
   });
+
+  it("should run exe script", () => {
+    cy.visit("/activity/script.html");
+    cy.contains("YAGNA_APPKEY").type("test_yagna_app_key");
+    cy.contains("AGREEMENT_ID").type("test_agreement_id");
+    cy.contains("Create Activity").click();
+    cy.contains("RUN SHELL COMMAND").type('echo "test_result"');
+    cy.contains("Add to script").click();
+    cy.contains("RUN SHELL COMMAND").type('echo "test_result 222"');
+    cy.contains("Add to script").click();
+    cy.contains("Execute").click();
+    cy.window().then((win) => {
+      // use this area to code using the variable, prepending with "win." eg.
+      cy.log(win.activity); // or console.log(win.varyingUrl)
+    });
+    cy.get("#results").should("include.text", "[stdout] test_result");
+  });
 });
