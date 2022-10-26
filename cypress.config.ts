a5d424ee6ec160f6cca147454095f4affa514953
Marcin Gordel <marcin.gordel@gmail.com>
2022-10-26 12:37:12 +0200

    Fixed cypres config with rollupjs

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,19 +4,35 @@ import path from "path";
 import { rollup } from "rollup";
 import stdLibBrowser from "node-stdlib-browser";
 import alias from "@rollup/plugin-alias";
+import * as ts from "typescript";
 
 // Mock Yagna API
 rollupConfig.plugins[0] = alias({
   entries: [
     ...Object.keys(stdLibBrowser).map((k) => ({ find: k, replacement: stdLibBrowser[k] })),
-    { find: /ya-ts-client\/dist\/ya-activity\/api$/, replacement: "./dist/tests/mock/activity_api.js" },
-    { find: "eventsource", replacement: "./dist/tests/mock/event_source.js" },
+    { find: /ya-ts-client\/dist\/ya-activity\/api$/, replacement: "./dist/mock/activity_api.js" },
+    { find: "eventsource", replacement: "./dist/mock/event_source.js" },
   ],
 });
 rollupConfig.output.file = path.resolve(__dirname, "./examples/web/js/bundle.js");
 
+function compileTSMocks() {
+  const options = {
+    target: ts.ScriptTarget.ESNext,
+    module: ts.ModuleKind.CommonJS,
+    moduleResolution: ts.ModuleResolutionKind.NodeJs,
+    outDir: "dist/mock",
+  };
+  const fileNames = [__dirname + "/tests/mock/activity_api.ts", __dirname + "/tests/mock/event_source.ts"];
+  const program = ts.createProgram(fileNames, options);
+  console.log("Trying to compile typescript mocks...");
+  const results = program.emit();
+  const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(results.diagnostics);
+  if (allDiagnostics.length > 0) throw new Error(JSON.stringify(allDiagnostics));
+  console.log("Typescript has been successfully compiled");
+}
+
 export default defineConfig({
-  projectId: "dm6cbd",
   fileServerFolder: "examples/web",
   supportFolder: "tests/cypress/support",
   fixturesFolder: "tests/cypress/fixtures",
@@ -27,6 +43,7 @@ export default defineConfig({
     specPattern: "tests/cypress/ui/**/*.cy.ts",
     setupNodeEvents(on) {
       on("before:run", async () => {
+        compileTSMocks();
         let bundle;
         try {
           console.log("Trying to compile bundle by rollup.js...");
@@ -37,7 +54,7 @@ export default defineConfig({
         }
         if (!bundle) throw new Error("Rollup bundle compilation error");
         await bundle.close();
-        console.log("Browser bundle compiled by rollup");
+        console.log("Browser bundle has been successfully compiled by rollup");
       });
     },
   },
