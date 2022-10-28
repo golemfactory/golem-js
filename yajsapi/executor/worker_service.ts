290c9659b6475f5db64179b14e31e2fd557a1507
Marcin Gordel <marcin.gordel@gmail.com>
2022-10-28 14:45:00 +0200

    Small fixes

--- yajsapi/executor/worker_service.ts
+++ yajsapi/executor/worker_service.ts
@@ -1,41 +1,60 @@
-import { sleep } from "../utils";
-import * as events from "./events";
-
-let isRunning, workersQueue, maxWorkers, agreementPool, eventBus, activityFactory, paymentsService, WorkContext;
+// TMP:
+let maxParallelTasks, WorkContext, sleep, events;
 
 class WorkerService {
-  private workersQueue;
-  private activeWorkers = new Set();
+  private isRunning;
+  private agreementPool;
+  private eventBus;
+  private activityFactory;
+  private paymentsService;
+  private tasksQueue;
+  private activeTasks = new Set();
   private activities = new Map();
+  private logger;
 
-  public get() {
-    // TODO: get worker from queue
+  public add(task) {
+    this.tasksQueue.add(task);
   }
-  public add() {
-    // TODO: add task/worker to queue
-  }
+
   public async run() {
-    while (isRunning) {
+    while (this.isRunning) {
+      const task = this.tasksQueue.get();
+      if (!task || this.activeTasks.size >= maxParallelTasks) continue;
+      this.startTask(task).catch((error) => this.logger(error));
       await sleep(2);
-      const worker = workersQueue.get();
-      if (!worker || this.activeWorkers.size >= maxWorkers) continue;
-      const agreement = await agreementPool.get();
-      eventBus.emit(new events.WorkerStarted({ agr_id: agreement.id }));
-      let activity;
-      if (!this.activities.has(agreement.id)) {
-        activity = activityFactory.create(agreement.id);
-        this.activities.set(agreement.id, activity.id);
-        eventBus.emit(new events.ActivityCreated({ act_id: activity.id, agr_id: agreement.id }));
-      } else {
-        activity = this.activities.get(agreement.id);
-        eventBus.emit(new events.ActivityReused({ act_id: activity.id, agr_id: agreement.id }));
-      }
-      paymentsService.acceptDebitNotesFor(agreement.id);
-      const workContext = new WorkContext(agreement, activity, worker);
-      await workContext.before();
-      await workContext.execute();
-      paymentsService.acceptInvoicesFor(agreement.id);
-      await agreementPool.releaseAgreement(agreement.id);
     }
   }
+
+  private async startTask(task) {
+    const agreement = await this.agreementPool.get();
+    this.eventBus.emit(new events.TaskStarted({ agr_id: agreement.id }));
+    let activity;
+    if (!this.activities.has(agreement.id)) {
+      activity = this.activityFactory.create(agreement.id);
+      this.activities.set(agreement.id, activity.id);
+      this.eventBus.emit(new events.ActivityCreated({ act_id: activity.id, agr_id: agreement.id }));
+    } else {
+      activity = this.activities.get(agreement.id);
+      this.eventBus.emit(new events.ActivityReused({ act_id: activity.id, agr_id: agreement.id }));
+    }
+    this.paymentsService.acceptDebitNotesFor(agreement.id);
+    const workContext = new WorkContext(agreement, activity, task);
+    await workContext.before();
+    await workContext.execute();
+    this.paymentsService.acceptInvoicesFor(agreement.id);
+    await this.agreementPool.releaseAgreement(agreement.id);
+    this.eventBus.emit(new events.TaskFinished({ task }));
+  }
 }
+
+// USAGE EXAMPLE:
+const workerService = new WorkerService();
+
+// in executor init:
+workerService.run();
+
+// executor.run(task):
+workerService.add({ simple_task_1: true });
+workerService.add({ simple_task_2: true });
+workerService.add({ simple_task_3: true });
+
