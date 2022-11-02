cc4fa82b2d576eae65a15b38d12abe26e91077a2
Krzysztof Cuber <k.cuber@braunmann.com>
2022-11-02 10:13:02 +0100

    First unit test structure for SmartQueue

---
+++ tests/unit/smartq.spec.ts
@@ -0,0 +1,12 @@
+import test from "ava";
+
+test.todo('allow to add Task')
+
+test.todo('get should Task and remove it from the queue')
+
+test.todo('add Task without any previous attempt to the end of queue')
+
+test.todo('add Task without any with any attempt to beginning of queue')
+
+test.todo('should return amount of tasks in the queue')
+
