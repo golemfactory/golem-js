727326ab8f6270f8437fb663b4e959380fad10da
Krzysztof Cuber <k.cuber@braunmann.com>
2022-11-02 14:50:17 +0100

    SmartQueue to TaskQueue refactor

---
+++ tests/mock/task_mock.ts
@@ -0,0 +1,27 @@
+export enum TaskState {
+    New,
+    Retry,
+    Pending,
+    Done,
+}
+
+export default class TaskMock {
+    constructor(private results, private state: TaskState) {
+    }
+
+    public isPending() {
+        return this.state == TaskState.Pending;
+    }
+
+    public isDone() {
+        return this.state == TaskState.Done;
+    }
+
+    public getResults() {
+        return this.results;
+    }
+
+    public isRetry() {
+        return this.state == TaskState.Retry;
+    }
+}
