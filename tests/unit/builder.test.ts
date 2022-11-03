2ae6a9c3748e8158de225ff44cc7a1d8592358c1
Krzysztof Cuber <k.cuber@braunmann.com>
2022-11-03 10:47:13 +0100

    Change test function to sync in DemandBuilder

--- tests/unit/builder.test.ts
+++ tests/unit/builder.test.ts
@@ -2,27 +2,24 @@ import { expect } from "chai";
 import { DemandBuilder } from "../../yajsapi/props";
 
 describe("#DemandBuilder()", () => {
-    it('empty constraints', done => {
+    it('empty constraints', () => {
         const demand_builder = new DemandBuilder();
         const constraints: string = demand_builder.constraints();
         expect(constraints).to.equal("(&)");
-        done();
     });
 
-    it('one constraint', done => {
+    it('one constraint', () => {
         const demand_builder = new DemandBuilder();
         demand_builder.ensure("(a=b)");
         const constraints: string = demand_builder.constraints();
         expect(constraints).to.equal("(a=b)");
-        done();
     });
 
-    it('two constraints', done => {
+    it('two constraints', () => {
         const demand_builder = new DemandBuilder();
         demand_builder.ensure("(a.b=c)");
         demand_builder.ensure("(d.e=f)");
         const constraints: string = demand_builder.constraints();
         expect(constraints).to.equal("(&(a.b=c)\n\t(d.e=f))");
-        done();
     });
 });
