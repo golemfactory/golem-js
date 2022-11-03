ac45ab1518407cbd42d8e8a3e8aac7a77d8778cb
Krzysztof Cuber <k.cuber@braunmann.com>
2022-11-03 11:53:42 +0100

    Prepare task service test for mocha

---
+++ tests/unit/tasks_service.test.ts
@@ -0,0 +1,19 @@
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+describe("#TaskService()", () => {
+    it("process new task in queue", async () => {
+        // TODO
+    });
+
+    it("process only allowed number of tasks", async () => {
+        // TODO
+    });
+
+    it("stop the service if it is interrupted by the user", async () => {
+        // TODO
+    });
+});
