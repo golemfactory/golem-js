2f49ce70493ae26288aa260a8a5af5fd3e824061
Marcin Gordel <marcin.gordel@gmail.com>
2022-11-14 13:25:34 +0100

    First draft (#404)

--- yajsapi/market/market_service.ts
+++ yajsapi/market/market_service.ts
@@ -1,5 +1,8 @@
 import { Logger } from "../utils";
 import { EventBus } from "../events/event_bus";
+import { Package } from "../package";
+import { Demand } from "./demand";
+import { Subscription } from "./subscription";
 
 export class MarketService {
   constructor(
@@ -7,11 +10,27 @@ export class MarketService {
     private readonly eventBus: EventBus,
     private readonly logger?: Logger
   ) {}
-  async run(taskPackage) {
+  async run(taskPackage: Package) {
     this.logger?.info("The Market Service has started");
+    const demand = await this.createDemand(taskPackage);
+    const subscriptionId = await this.publishDemand(demand);
+    const subscription = this.createSubscription(subscriptionId);
+    subscription.on("offer", this.eventBus.emit);
   }
 
   async end() {
     // todo
+  }
+
+  private async createDemand(taskPackage: Package): Promise<Demand> {
+    return new Demand();
+  }
+
+  private async publishDemand(demand: Demand): Promise<string> {
+    return "todo";
+  }
+
+  private createSubscription(subscriptionId: string): Subscription {
+    return new Subscription();
   }
 }
