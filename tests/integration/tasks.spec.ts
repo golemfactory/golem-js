667f298e061070aac49cf133fb94941393f8596a
Marcin Gordel <marcin.gordel@gmai.com>
2022-12-22 13:33:35 +0000

    Fixed bug blender test (#422)

--- tests/integration/tasks.spec.ts
+++ tests/integration/tasks.spec.ts
@@ -78,62 +78,64 @@ describe("Task Executor", function () {
     await executor.end();
   }).timeout(60000);
 
-  // it("should run simple batch script and get results as stream", async () => {
-  //   const executor = await createExecutor({
-  //     package: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
-  //     yagnaOptions: { apiKey, basePath },
-  //     subnetTag,
-  //     logger,
-  //   });
-  //   const outputs: string[] = [];
-  //   let onEnd = "";
-  //   await executor
-  //     .run(async (ctx) => {
-  //       const results = await ctx
-  //         .beginBatch()
-  //         .run('echo "Hello Golem"')
-  //         .run('echo "Hello World"')
-  //         .run('echo "OK"')
-  //         .endStream();
-  //       results.on("data", ({ stdout }) => outputs.push(stdout?.trim()));
-  //       results.on("close", () => (onEnd = "END"));
-  //     })
-  //     .catch((e) => {
-  //       executor.end();
-  //       expect(e).to.be.undefined;
-  //     });
-  //   expect(outputs[0]).to.equal("Hello Golem");
-  //   expect(outputs[1]).to.equal("Hello World");
-  //   expect(outputs[2]).to.equal("OK");
-  //   expect(onEnd).to.equal("END");
-  //   await executor.end();
-  // }).timeout(60000);
+  it("should run simple batch script and get results as stream", async () => {
+    const executor = await createExecutor({
+      package: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
+      yagnaOptions: { apiKey, basePath },
+      subnetTag,
+      logger,
+    });
+    const outputs: string[] = [];
+    let onEnd = "";
+    await executor
+      .run(async (ctx) => {
+        const results = await ctx
+          .beginBatch()
+          .run('echo "Hello Golem"')
+          .run('echo "Hello World"')
+          .run('echo "OK"')
+          .endStream();
+        results.on("data", ({ stdout }) => outputs.push(stdout?.trim()));
+        results.on("close", () => (onEnd = "END"));
+      })
+      .catch((e) => {
+        executor.end();
+        expect(e).to.be.undefined;
+      });
+    await logger.expectToInclude('Task 1 finished', 5000)
+    expect(outputs[0]).to.equal("Hello Golem");
+    expect(outputs[1]).to.equal("Hello World");
+    expect(outputs[2]).to.equal("OK");
+    expect(onEnd).to.equal("END");
+    await executor.end();
+  }).timeout(80000);
 
-  // it("should run simple batch script and catch error on stream", async () => {
-  //   const executor = await createExecutor({
-  //     package: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
-  //     yagnaOptions: { apiKey, basePath },
-  //     subnetTag,
-  //     logger,
-  //   });
-  //   const outputs: string[] = [];
-  //   let expectedError = "";
-  //   await executor
-  //     .run(async (ctx) => {
-  //       const results = await ctx.beginBatch().run('echo "Hello Golem"').run('echo "invalid_command"').endStream();
-  //       results.on("data", ({ stdout }) => outputs.push(stdout?.trim()));
-  //       results.on("error", (error) => {
-  //         expectedError = error.toString();
-  //       });
-  //     })
-  //     .catch((e) => {
-  //       executor.end();
-  //       expect(e).to.be.undefined;
-  //     });
-  //   expect(outputs[0]).to.equal("Hello Golem");
-  //   expect(expectedError).to.equal("error");
-  //   await executor.end();
-  // }).timeout(60000);
+  it("should run simple batch script and catch error on stream", async () => {
+    const executor = await createExecutor({
+      package: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
+      yagnaOptions: { apiKey, basePath },
+      subnetTag,
+      logger,
+    });
+    const outputs: string[] = [];
+    let expectedError = "";
+    await executor
+      .run(async (ctx) => {
+        const results = await ctx.beginBatch().run('echo "Hello Golem"').run('invalid_command').endStream();
+        results.on("data", ({ stdout }) => outputs.push(stdout?.trim()));
+        results.on("error", (error) => {
+          expectedError = error.toString();
+        });
+      })
+      .catch((e) => {
+        executor.end();
+        expect(e).to.be.undefined;
+      });
+    await logger.expectToInclude('Task 1 finished', 5000)
+    expect(outputs[0]).to.equal("Hello Golem");
+    expect(expectedError).to.equal("Error: ExeScript command exited with code 127. Stdout: undefined. Stderr: sh: 1: invalid_command: not found");
+    await executor.end();
+  }).timeout(80000);
 
   it("should run simple batch script and get results as promise", async () => {
     const executor = await createExecutor({
