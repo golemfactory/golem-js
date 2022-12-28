396bed7c119043e3f5d5e05ba5243adda834bcc0
Marcin Gordel <marcin.gordel@gmai.com>
2022-12-28 14:53:32 +0000

    Added ssh and yacat integrations tests (#422)

---
+++ tests/integration/ssh.spec.ts
@@ -0,0 +1,63 @@
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { createExecutor } from "../../yajsapi";
+import { LoggerMock } from "../mock";
+import { Goth } from "./goth";
+import { resolve } from "path";
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+const logger = new LoggerMock(false);
+import crypto from "crypto";
+import { spawn } from "child_process"
+
+describe("SSH connection", function () {
+    // TODO
+  const sshConnectionCheck = false;
+  it("should connect to provider via ssh", async () => {
+      const executor = await createExecutor({
+          package: "1e06505997e8bd1b9e1a00bd10d255fc6a390905e4d6840a22a79902",
+          capabilities: ["vpn"],
+          networkAddress: "192.168.0.0/24",
+          logger
+      });
+      let websocketUri;
+      const password = crypto.randomBytes(3).toString("hex");
+      await executor.run(async (ctx) => {
+        websocketUri = ctx.getWebsocketUri(22);
+        const results = await ctx
+          .beginBatch()
+          .run("syslogd")
+          .run("ssh-keygen -A")
+          .run(`echo -e "${password}\n${password}" | passwd`)
+          .run("/usr/sbin/sshd")
+          .end()
+          .catch((e) => console.error(e));
+        expect(results?.[3]?.result).to.equal('Ok');
+        expect(websocketUri).to.an('string');
+        if (sshConnectionCheck) {
+            const processSsh = spawn('ssh', [
+                '-o',
+                "UserKnownHostsFile=/dev/null",
+                "-o",
+                "StrictHostKeyChecking=no",
+                '-o',
+                `ProxyCommand='websocat asyncstdio: ${websocketUri} --binary -H=Authorization:"Bearer ${process.env.YAGNA_APPKEY}"'`,
+                `root@${crypto.randomBytes(10).toString("hex")}`,
+                "uname -v",
+            ]);
+            let error, stdout;
+            processSsh.stdout?.setEncoding('utf-8');
+            processSsh.stderr?.setEncoding('utf-8');
+            processSsh.stdout.on('data', data => (stdout = data));
+            processSsh.stderr.on('data', data => (error = data));
+            processSsh.on('error', (data) => (error = data));
+            processSsh.stdin?.write(password);
+            processSsh.stdin?.end();
+            await new Promise(res => setTimeout(res, 2000));
+            expect(stdout).to.include('1-Alpine SMP');
+            expect(error).to.an('undefined');
+        }
+      });
+      await executor.end();
+  }).timeout(180000);
+});
