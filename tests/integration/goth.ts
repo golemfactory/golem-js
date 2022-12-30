79145672a4d3bf009bc690f69e59217fc1b99a1e
Marcin Gordel <marcin.gordel@gmail.com>
2022-12-30 16:44:57 +0100

    CI test (#422)

--- tests/integration/goth.ts
+++ tests/integration/goth.ts
@@ -1,40 +1,46 @@
-import {ChildProcess, spawn} from "child_process";
+import { ChildProcess, spawn } from "child_process";
 import fs from "fs";
-import {Readable, Writable} from "stream";
+import { Readable, Writable } from "stream";
 
 export class Goth {
   private gothProcess?: ChildProcess;
 
-  constructor(private readonly gothConfig) {
-  }
-  async start(): Promise<{ apiKey: string; basePath: string; subnetTag: string, gsbUrl: string }> {
+  constructor(private readonly gothConfig) {}
+  async start(): Promise<{ apiKey: string; basePath: string; subnetTag: string; gsbUrl: string; path: string }> {
     return new Promise((resolve, reject) => {
       const startTime = Date.now();
       console.log("\x1b[33mStarting goth process...\x1b[0m");
-      console.log("\x1b[33mRun command:\x1b[0m \x1b[36m", `python -m goth start ${this.gothConfig}`, '\x1b[0m');
-      this.gothProcess = spawn("python", ["-m", "goth", "start", this.gothConfig], { env: { ...process.env, PYTHONUNBUFFERED: "1" } });
-      this.gothProcess?.stdout?.setEncoding('utf-8');
-      this.gothProcess?.stderr?.setEncoding('utf-8');
+      console.log("\x1b[33mRun command:\x1b[0m \x1b[36m", `python -m goth start ${this.gothConfig}`, "\x1b[0m");
+      this.gothProcess = spawn("python", ["-m", "goth", "start", this.gothConfig], {
+        env: { ...process.env, PYTHONUNBUFFERED: "1" },
+      });
+      this.gothProcess?.stdout?.setEncoding("utf-8");
+      this.gothProcess?.stderr?.setEncoding("utf-8");
       this.gothProcess?.stdout?.on("data", (data) => {
         const regexp =
-            /YAGNA_APPKEY=(\w+) YAGNA_API_URL=(http:\/\/127\.0{0,3}\.0{0,3}.0{0,2}1:\d+) GSB_URL=(tcp:\/\/\d+\.\d+\.\d+\.\d+:\d+).*YAGNA_SUBNET=(\w+)/g;
+          /YAGNA_APPKEY=(\w+) YAGNA_API_URL=(http:\/\/127\.0{0,3}\.0{0,3}.0{0,2}1:\d+) GSB_URL=(tcp:\/\/\d+\.\d+\.\d+\.\d+:\d+) PATH=(.*) YAGNA_SUBNET=(\w+)/g;
         const results = Array.from(data?.toString()?.matchAll(regexp) || [])?.pop();
         const apiKey = results?.[1];
         const basePath = results?.[2];
         const gsbUrl = results?.[3];
-        const subnetTag = results?.[4];
+        const path = results?.[4];
+        const subnetTag = results?.[5];
         if (apiKey) {
-          process.env['YAGNA_APPKEY'] = apiKey;
-          process.env['YAGNA_API_URL'] = basePath;
-          process.env['GSB_URL'] = gsbUrl;
-          process.env['YAGNA_SUBNET'] = subnetTag;
-          console.log(`\x1b[33mGoth has been successfully started in ${((Date.now() - startTime)/ 1000).toFixed(0)} s.\n`);
-          resolve({ apiKey, basePath, subnetTag, gsbUrl });
+          process.env["YAGNA_APPKEY"] = apiKey;
+          process.env["YAGNA_API_URL"] = basePath;
+          process.env["GSB_URL"] = gsbUrl;
+          process.env["PATH"] = path;
+          process.env["YAGNA_SUBNET"] = subnetTag;
+          console.log(
+            `\x1b[33mGoth has been successfully started in ${((Date.now() - startTime) / 1000).toFixed(0)}s.\n`
+          );
+          console.log({ apiKey, basePath, subnetTag, gsbUrl, path });
+          resolve({ apiKey, basePath, subnetTag, gsbUrl, path });
         }
       });
       this.gothProcess?.stderr?.on("data", (data) => {
         if (data.toString().match(/error/)) reject(data);
-        console.log("\x1b[33m[goth]\x1b[0m " + data.replace(/[\n\t\r]/g,""));
+        console.log("\x1b[33m[goth]\x1b[0m " + data.replace(/[\n\t\r]/g, ""));
       });
       this.gothProcess.on("error", (error) => reject(error.toString()));
       this.gothProcess.on("close", (code) => reject(`Goth process exit with code ${code}`));
@@ -42,7 +48,7 @@ export class Goth {
     });
   }
   async end() {
-    this.gothProcess?.kill('SIGINT');
+    this.gothProcess?.kill("SIGINT");
     return new Promise((resolve) => {
       this.gothProcess?.on("close", () => resolve(console.log(`\x1b[33mGoth has been terminated`)));
     });
