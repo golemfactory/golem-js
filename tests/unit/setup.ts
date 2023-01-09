2d1b4610b48c3aeefbe998f2aa144df6e3b8f911
Marcin Gordel <marcin.gordel@gmail.com>
2023-01-09 13:04:28 +0100

    Fixed unit test on windows os (#354)

---
+++ tests/unit/setup.ts
@@ -0,0 +1,15 @@
+import rewiremock from "rewiremock";
+import * as activityMock from "../mock/rest/activity";
+import { MarketApiMock } from "../mock/rest/market";
+import EventSourceMock from "../mock/utils/event_source";
+import { PaymentApiMock } from "../mock/rest/payment";
+import { NetworkApiMock } from "../mock/rest/network";
+rewiremock("ya-ts-client/dist/ya-net/api").with({ RequestorApi: NetworkApiMock });
+rewiremock("ya-ts-client/dist/ya-payment/api").with({ RequestorApi: PaymentApiMock });
+rewiremock("ya-ts-client/dist/ya-activity/api").with({
+  RequestorControlApi: activityMock.RequestorControlApiMock,
+  RequestorStateApi: activityMock.RequestorSateApiMock,
+});
+rewiremock("eventsource").with(EventSourceMock);
+rewiremock("ya-ts-client/dist/ya-market/api").with({ RequestorApi: MarketApiMock });
+rewiremock.enable();
