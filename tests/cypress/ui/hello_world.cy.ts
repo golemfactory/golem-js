f2e1323e4752ca00f1e13e03ee49e20192058308
Marcin Gordel <marcin.gordel@gmail.com>
2023-01-12 12:18:26 +0100

    Fixed cypress tests (#354)

--- tests/cypress/ui/hello_world.cy.ts
+++ tests/cypress/ui/hello_world.cy.ts
@@ -6,5 +6,6 @@ describe("Test TaskExecutor API", () => {
     cy.get("#SUBNET_TAG").clear().type(Cypress.env("YAGNA_SUBNET"));
     cy.get("#echo").click();
     cy.get("#results").should("include.text", "Hello World", { timeout: 60000 });
+    cy.get("#logs").contains("Task Executor has shut down");
   });
 });
