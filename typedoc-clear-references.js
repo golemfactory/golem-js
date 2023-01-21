dec68b9a0c7f933dc616ecefa5b27eb8f46b8916
Krzysztof Cuber <k.cuber@braunmann.com>
2023-01-21 15:28:24 +0000

    Typedoc custom plugin

---
+++ typedoc-clear-references.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.load = void 0;
+const typedoc = require("typedoc");
+function load({ application }) {
+  application.converter.on(typedoc.Converter.EVENT_RESOLVE_BEGIN, (context) => {
+    for (const reflection of context.project.getReflectionsByKind(typedoc.ReflectionKind.Reference)) {
+      context.project.removeReflection(reflection);
+    }
+    for (const reflection of context.project.getReflectionsByKind(typedoc.ReflectionKind.Module)) {
+      if (reflection.children.length == 0) {
+        context.project.removeReflection(reflection);
+      }
+    }
+  });
+}
+exports.load = load;
