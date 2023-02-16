c27847c7b503f7dfe303471213cc34cac1b76736
Krzysztof Cuber <k.cuber@braunmann.com>
2023-02-16 11:31:56 +0100

    map loop form 10 to 5items

--- examples/simple-usage/forEach.ts
+++ examples/simple-usage/forEach.ts
@@ -2,7 +2,7 @@ import { TaskExecutor } from "yajsapi";
 
 (async function main() {
   const executor = await TaskExecutor.create("9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae");
-  const data = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+  const data = ["one", "two", "three", "four", "five"];
   await executor.forEach(data, async (ctx, x) => {
     const res = await ctx.run(`echo "${x}"`);
     console.log(`Result=${res.stdout}`);
