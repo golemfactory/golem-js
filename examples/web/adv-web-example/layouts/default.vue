587d99203661c458dd56fcc73b1c96f41c4026e6
Marcin Gordel <marcin.gordel@gmail.com>
2023-02-23 10:49:56 +0100

    Home Dashboard and styles (#382)

--- examples/web/adv-web-example/layouts/default.vue
+++ examples/web/adv-web-example/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="main-layout">
     <el-container>
-      <el-header><TheHeader /></el-header>
+      <el-header height="50px"><TheHeader /></el-header>
       <el-main><slot /></el-main>
       <el-footer><TheFooter /></el-footer>
     </el-container>
--- examples/web/adv-web-example/pages/index.vue
+++ examples/web/adv-web-example/pages/index.vue
@@ -1,16 +1,70 @@
 <template>
-  <el-card>
-    <h1>Run your Lambda on Golem Network</h1>
-    <div>
-      <NuxtLink v-slot="{ navigate }" to="/simple" custom>
-        <el-button type="primary" @click="navigate">Simple way</el-button>
+  <div class="home">
+    <h1>Run JavaScript code<br />on the <NuxtLink to="https://golem.network">Golem Network</NuxtLink></h1>
+    <video class="script" src="~/assets/video/script.mp4" autoplay loop></video>
+    <div class="buttons">
+      <NuxtLink v-slot="{ navigate }" to="/task" custom>
+        <el-button size="large" class="btn-task" @click="navigate"><span>Just RUN<br/><i>(Task API)</i></span></el-button>
       </NuxtLink>
-      <NuxtLink v-slot="{ navigate }" to="/advanced" custom>
-        <el-button type="success" @click="navigate">Advanced way</el-button>
-      </NuxtLink>
-      <NuxtLink v-slot="{ navigate }" to="/wrapper-test" custom>
-        <el-button type="danger" @click="navigate">YaJSAPI</el-button>
+      <NuxtLink v-slot="{ navigate }" to="/mid" custom>
+        <el-button size="large" class="btn-mid" plain @click="navigate"><span>Set and control each step manually<br/><i>(Mid-level API)</i></span></el-button>
       </NuxtLink>
     </div>
-  </el-card>
+
+  </div>
+
 </template>
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  margin: 30px 0;
+  font-size: 2.8rem;
+  line-height: 3.5rem;
+  text-align: center;
+  a {
+
+  }
+}
+.script {
+  height: 300px;
+  padding-bottom: 30px;
+}
+.buttons {
+  min-width: 530px;
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 30px;
+  button {
+    flex-direction: column;
+    line-height: 1.4rem;
+    font-size: 1rem;
+    height: 70px;
+    i {
+      font-style: italic;
+      font-size: .7rem;
+    }
+  }
+  .btn-task {
+    min-width: 200px;
+    color: white;
+    background: #58a51b;
+    border-color: #58a51b;
+  }
+  .btn-mid {
+    background: #26242f;
+    color: darkorange;
+    border-color: darkorange;
+  }
+}
+</style>
