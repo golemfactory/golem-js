8d366231e21e2c9f0d4cc255368c4cac713c0b33
Marcin Gordel <marcin.gordel@gmail.com>
2023-03-07 10:12:24 +0100

    Replaced winston to pino (#467)

--- yajsapi/utils/index.ts
+++ yajsapi/utils/index.ts
@@ -1,7 +1,7 @@
-import sleep from './sleep.js';
-import { Logger } from './logger.js';
-import { winstonLogger } from './winstonLogger.js';
-import { ConsoleLogger } from './consoleLogger.js';
-import * as runtimeContextChecker from './runtimeContextChecker.js';
+import sleep from "./sleep.js";
+import { Logger } from "./logger.js";
+import { pinoLogger } from "./pinoLogger.js";
+import { ConsoleLogger } from "./consoleLogger.js";
+import * as runtimeContextChecker from "./runtimeContextChecker.js";
 
-export { sleep, Logger, winstonLogger, runtimeContextChecker, ConsoleLogger };
+export { sleep, Logger, runtimeContextChecker, pinoLogger, ConsoleLogger };
---
+++ yajsapi/utils/pinoLogger.ts
@@ -0,0 +1,29 @@
+import { pino } from "pino";
+import PinoPretty from "pino-pretty";
+import { Logger } from "./logger.js";
+
+const logger = pino({
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+});
+let logLevel;
+/**
+ *
+ * @ignore
+ */
+export const pinoLogger: Logger = {
+  level: logLevel,
+  debug: (msg) => logger.debug(msg),
+  info: (msg) => logger.info(msg),
+  log: (msg) => logger.info(msg),
+  warn: (msg) => logger.warn(msg),
+  error: (msg) => logger.error(msg),
+  table: (object) => console.table(object),
+  setLevel: (level: string) => {
+    logger.level = level;
+  },
+};
