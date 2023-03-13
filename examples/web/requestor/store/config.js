2687dc6cd201e1c30cdf200398d1c4ba65d2169f
Marcin Gordel <marcin.gordel@gmail.com>
2023-03-13 15:49:07 +0100

    Removed api key (#363)

--- examples/web/requestor/store/config.js
+++ examples/web/requestor/store/config.js
@@ -5,7 +5,7 @@ export const useConfigStore = defineStore("config", {
     options: {
       imageHash: "3ddb225352af0c2f44dfa9d5b59b4f197670aac73bdd323d137751dc",
       yagnaOptions: {
-        apiKey: "30c59fef7d8c4639b62d576bfb624e1a",
+        apiKey: "",
         basePath: "http://127.0.0.1:7465",
       },
       taskTimeout: 1000 * 60 * 3,
