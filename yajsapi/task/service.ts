e208e954c1a9fbce2c6966e4ab8bc4ac6459cfd4
Marcin Gordel <marcin.gordel@gmail.com>
2023-03-31 21:26:49 +0200

    Task service fix

--- yajsapi/task/service.ts
+++ yajsapi/task/service.ts
@@ -127,7 +127,7 @@ export class TaskService {
         this.options.eventTarget?.dispatchEvent(new Events.TaskRejected({ id: task.id, reason }));
         throw new Error(`Task ${task.id} has been rejected! ${reason}`);
       }
-      await activity.stop().catch((actError) => this.logger?.error(actError));
+      await activity?.stop().catch((actError) => this.logger?.error(actError));
       this.activities.delete(agreement.id);
     } finally {
       --this.activeTasksCount;
