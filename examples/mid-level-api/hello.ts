8b3db887154b4bf4d575c9a11f2b41d036efe41b
Marcin Gordel <marcin.gordel@gmail.com>
2023-06-13 16:06:58 +0200

    Fixed examples default params

--- examples/mid-level-api/hello.ts
+++ examples/mid-level-api/hello.ts
@@ -23,7 +23,7 @@ async function main() {
   const logger = new ConsoleLogger();
   const taskPackage = await Package.create({ imageHash: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae" });
   const accounts = await (await Accounts.create()).list();
-  const account = accounts.find((account) => account?.platform.indexOf("erc20") !== -1);
+  const account = accounts.find((account) => account?.platform.indexOf("erc20-goerli") !== -1);
   if (!account) throw new Error("There is no available account");
   const allocation = await Allocation.create({ account, logger });
   const demand = await Demand.create(taskPackage, [allocation], { logger });
