323277dc9b4df852697f462ae325a480c945ecfb
Marcin Gordel <marcin.gordel@gmail.com>
2023-06-30 12:57:58 +0200

    Added integration tests for market service filters (#510)

---
+++ tests/integration/startegy.ts
@@ -0,0 +1,64 @@
+import { expect } from "chai";
+import { TaskExecutor, ProposalFilters, PaymentFilters } from "../../yajsapi/index.js";
+import { LoggerMock } from "../mock/index.js";
+
+const logger = new LoggerMock(false);
+
+const gothProviders = [
+  { id: "", name: "provider-1" },
+  { id: "", name: "provider-2" },
+];
+
+describe("Strategies", function () {
+  let executor: TaskExecutor;
+  afterEach(async function () {
+    this.timeout(60000);
+    await executor.end();
+  });
+  describe("Proposals", () => {
+    it("should filtered providers by black list names", async () => {
+      executor = await TaskExecutor.create({
+        package: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
+        payment: { network: "rinkeby" },
+        proposalFilter: ProposalFilters.BlackListProposalNamesFilter(/provider-2/),
+        logger,
+      });
+      const data = ["one", "two", "three"];
+      const results = executor.map<string, string>(data, async (ctx, x) => {
+        const res = await ctx.run(`echo "${x}"`);
+        return res.stdout?.trim();
+      });
+      const finalOutputs: string[] = [];
+      for await (const res of results) if (res) finalOutputs.push(res);
+      expect(finalOutputs).to.have.members(data);
+      await logger.expectToInclude(`Task 1 computed by provider ${gothProviders[0].name}`, 5000);
+      await logger.expectToInclude(`Task 2 computed by provider ${gothProviders[1].name}`, 5000);
+      await logger.expectToInclude(`Task 3 computed by provider ${gothProviders[2].name}`, 5000);
+      await logger.expectToInclude(`Proposal rejected by Proposal Filter`, 5000);
+    });
+
+    it("should filtered providers by white list names", async () => {
+      executor = await TaskExecutor.create({
+        package: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
+        payment: { network: "rinkeby" },
+        proposalFilter: ProposalFilters.WhiteListProposalNamesFilter(/provider-1/),
+        logger,
+      });
+      const data = ["one", "two", "three"];
+      const results = executor.map<string, string>(data, async (ctx, x) => {
+        const res = await ctx.run(`echo "${x}"`);
+        return res.stdout?.trim();
+      });
+      const finalOutputs: string[] = [];
+      for await (const res of results) if (res) finalOutputs.push(res);
+      expect(finalOutputs).to.have.members(data);
+      await logger.expectToInclude(`Task 1 computed by provider ${gothProviders[0].name}`, 5000);
+      await logger.expectToInclude(`Task 2 computed by provider ${gothProviders[1].name}`, 5000);
+      await logger.expectToInclude(`Task 3 computed by provider ${gothProviders[2].name}`, 5000);
+      await logger.expectToInclude(`Proposal rejected by Proposal Filter`, 5000);
+    });
+  });
+  describe("Payments", () => {
+    // tood
+  });
+});
