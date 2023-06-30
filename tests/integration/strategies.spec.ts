7017bcca2efcfcc18522b6dfead06beb4b22d5d9
Marcin Gordel <marcin.gordel@gmail.com>
2023-06-30 14:25:32 +0200

    Added integration tests for market service filters (#510)

--- tests/integration/strategies.spec.ts
+++ tests/integration/strategies.spec.ts
@@ -5,14 +5,9 @@ import { LoggerMock } from "../mock/index.js";
 const logger = new LoggerMock(false);
 
 describe("Strategies", function () {
-  let executor: TaskExecutor;
-  afterEach(async function () {
-    this.timeout(60000);
-    await executor.end();
-  });
   describe("Proposals", () => {
     it("should filtered providers by black list names", async () => {
-      executor = await TaskExecutor.create({
+      const executor = await TaskExecutor.create({
         package: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
         payment: { network: "rinkeby" },
         proposalFilter: ProposalFilters.BlackListProposalNamesFilter(/provider-2/),
@@ -30,10 +25,11 @@ describe("Strategies", function () {
       await logger.expectToInclude(`Task 1 computed by provider provider-1`, 5000);
       await logger.expectToInclude(`Task 2 computed by provider provider-1`, 5000);
       await logger.expectToInclude(`Task 3 computed by provider provider-1`, 5000);
+      await executor.end();
     }).timeout(80000);
 
     it("should filtered providers by white list names", async () => {
-      executor = await TaskExecutor.create({
+      const executor = await TaskExecutor.create({
         package: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
         payment: { network: "rinkeby" },
         proposalFilter: ProposalFilters.WhiteListProposalNamesFilter(/provider-2/),
@@ -51,11 +47,12 @@ describe("Strategies", function () {
       await logger.expectToInclude(`Task 1 computed by provider provider-2`, 5000);
       await logger.expectToInclude(`Task 2 computed by provider provider-2`, 5000);
       await logger.expectToInclude(`Task 3 computed by provider provider-2`, 5000);
+      await executor.end();
     }).timeout(80000);
   });
   describe("Payments", () => {
     it("should accept invoices only below 0.00001 GLM", async () => {
-      executor = await TaskExecutor.create({
+      const executor = await TaskExecutor.create({
         package: "9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
         payment: { network: "rinkeby" },
         invoiceFilter: PaymentFilters.AcceptMaxAmountInvoiceFilter(0.00001),
@@ -69,6 +66,7 @@ describe("Strategies", function () {
       const finalOutputs: string[] = [];
       for await (const res of results) if (res) finalOutputs.push(res);
       expect(finalOutputs).to.have.members(data);
+      await executor.end();
       await logger.expectToInclude(
         `Invoice has been rejected for provider provider-1. Reason: Invoice rejected by Invoice Filter`,
         20000
