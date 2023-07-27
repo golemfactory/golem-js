5e8aaf68fcd4a3601d3d64902356b457de853772
Marcin Gordel <marcin.gordel@gmail.com>
2023-07-27 12:18:58 +0200

    WS Cypress test (JST-22)

--- tests/cypress/ui/image.cy.ts
+++ tests/cypress/ui/image.cy.ts
@@ -5,9 +5,11 @@ describe("Test TaskExecutor API", () => {
     cy.get("#YAGNA_API_BASEPATH").clear().type(Cypress.env("YAGNA_API_BASEPATH"));
     cy.get("#SUBNET_TAG").clear().type(Cypress.env("YAGNA_SUBNET"));
     cy.get("#PAYMENT_NETWORK").clear().type("rinkeby");
-    cy.get("#MEME_IMG").selectFile("image.jpg");
-    cy.get("#echo").click();
-    cy.get("#results").should("include.text", "Hello World", { timeout: 60000 });
+    cy.fixture("golem.png", { encoding: null }).as("imageFile");
+    cy.get("#MEME_IMG").selectFile("@imageFile");
+    cy.get("#RUN").click();
+    cy.get("#RESULT_MEME").should("include.src", "data:image/png;base64,todo", { timeout: 60000 });
+    cy.get("#logs").contains("computed by provider");
     cy.get("#logs").contains("Task Executor has shut down");
   });
 });
