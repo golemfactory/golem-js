dc9044150e67db8add07276c3442cf475fc7f27b
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2023-07-27 15:20:01 +0200

    JST-112 - Removing more .js extensions...

--- yajsapi/stats/tasks.ts
+++ yajsapi/stats/tasks.ts
@@ -1,4 +1,4 @@
-import { AbstractAggregator, ItemInfo } from './abstract_aggregator.js';
+import { AbstractAggregator, ItemInfo } from './abstract_aggregator';
 
 export enum TaskStatusEnum {
   Pending = "pending",
