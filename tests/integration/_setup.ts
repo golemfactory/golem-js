b99ff3181791bf8aca45063609a39d3434e772eb
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2023-07-28 00:57:23 +0200

    JST-112: Increase allowed timeout for Goth to 180s

--- tests/integration/_setup.ts
+++ tests/integration/_setup.ts
@@ -1,18 +1,16 @@
-import {Goth} from "../goth/goth";
-import {resolve} from "path";
+import { Goth } from "../goth/goth";
+import { resolve } from "path";
 
 const gothConfig = resolve("../goth/assets/goth-config.yml");
 
-// assert(gothConfig, "Goth was not configured correctly, please run `python -m goth create-assets tests/goth/assets`");
-
-jest.setTimeout(5 * 1000)
+jest.setTimeout(180 * 1000);
 
 const goth = new Goth(gothConfig);
 
 beforeAll(async function () {
-    await goth.start();
+  await goth.start();
 });
 
 afterAll(async function () {
-    await goth.end();
+  await goth.end();
 });
