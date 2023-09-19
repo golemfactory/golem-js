0931959abcc9676b7da2234af58b7e469658a68a
Marcin Gordel <marcin.gordel@gmail.com>
2023-09-19 15:47:43 +0200

    test(examples): added assertions for errors

--- tests/examples/examples.test.ts
+++ tests/examples/examples.test.ts
@@ -1,4 +1,4 @@
-import { spawn } from "child_process";
+import { ChildProcess, spawn } from "child_process";
 import { dirname, basename, resolve } from "path";
 import { Goth } from "../goth/goth";
 
@@ -14,13 +14,13 @@ type Example = {
 
 const examples: Example[] = [
   { cmd: "node", path: "examples/docs-examples/examples/composing-tasks/batch-end.mjs" },
-  { cmd: "node", path: "docs-examples/examples/composing-tasks/batch-endstream-chunks.mjs" },
-  { cmd: "node", path: "docs-examples/examples/composing-tasks/batch-endstream-forawait.mjs" },
-  { cmd: "node", path: "docs-examples/examples/composing-tasks/multiple-run-prosaic.mjs" },
-  { cmd: "node", path: "docs-examples/examples/composing-tasks/singleCommand.mjs" },
+  { cmd: "node", path: "examples/docs-examples/examples/composing-tasks/batch-endstream-chunks.mjs" },
+  { cmd: "node", path: "examples/docs-examples/examples/composing-tasks/batch-endstream-forawait.mjs" },
+  { cmd: "node", path: "examples/docs-examples/examples/composing-tasks/multiple-run-prosaic.mjs" },
+  { cmd: "node", path: "examples/docs-examples/examples/composing-tasks/singleCommand.mjs" },
 ];
 
-const criticalLogsRegexp = [/Task timeot/, /Task rejected/, /TODO/];
+const criticalLogsRegexp = [/Task timeot/, /Task *. has been rejected/, /ERROR: TypeError/];
 
 const timeoutPromise = (seconds: number) =>
   new Promise((res, rej) =>
@@ -29,12 +29,19 @@ const timeoutPromise = (seconds: number) =>
       seconds * 1000,
     ),
   );
-
+const spawnedExamples: ChildProcess[] = [];
 async function examplesTest(cmd: string, path: string, args: string[] = [], timeout = 120) {
   const file = basename(path);
   const cwd = dirname(path);
   const spawnedExample = spawn(cmd, [file, ...args], { stdio: "inherit", cwd });
+  spawnedExamples.push(spawnedExample);
+  spawnedExample.stdout?.setEncoding("utf-8");
   const testPromise = new Promise((res, rej) => {
+    spawnedExample.stdout?.on("data", (data: string) => {
+      if (criticalLogsRegexp.some((regexp) => data.match(regexp))) {
+        return rej(`Example test "${file}" failed.`);
+      }
+    });
     spawnedExample.on("close", (code, signal) => {
       if (code === 0) return res(true);
       rej(`Example test "${file}" exited with code ${code} by signal ${signal}`);
@@ -53,6 +60,9 @@ async function testAll(examples: Example[]) {
   } catch (error) {
     console.error(error);
     process.exit(1);
+  } finally {
+    await goth.end().catch((error) => console.error(error));
+    spawnedExamples.forEach((example) => example?.kill());
   }
   process.exit(0);
 }
