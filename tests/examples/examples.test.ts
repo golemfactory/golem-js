79cf8c18a8d53cfd671fa3c87dc7e2a3df2b9aed
Marcin Gordel <marcin.gordel@gmail.com>
2023-09-19 12:49:14 +0200

    test(examples): goth ci debug

--- tests/examples/examples.test.ts
+++ tests/examples/examples.test.ts
@@ -5,7 +5,14 @@ import { Goth } from "../goth/goth";
 const gothConfig = resolve("../goth/assets/goth-config.yml");
 const goth = new Goth(gothConfig);
 
-const examples = [
+type Example = {
+  cmd: string;
+  path: string;
+  args?: string[];
+  timeout?: number;
+};
+
+const examples: Example[] = [
   { cmd: "node", path: "examples/docs-examples/examples/composing-tasks/batch-end.mjs" },
   { cmd: "node", path: "docs-examples/examples/composing-tasks/batch-endstream-chunks.mjs" },
   { cmd: "node", path: "docs-examples/examples/composing-tasks/batch-endstream-forawait.mjs" },
@@ -15,7 +22,7 @@ const examples = [
 
 const criticalLogsRegexp = [/Task timeot/, /Task rejected/, /TODO/];
 
-const timeoutPromise = (seconds) =>
+const timeoutPromise = (seconds: number) =>
   new Promise((res, rej) =>
     setTimeout(
       () => rej(new Error(`The timeout was reached and the racing promise has rejected after ${seconds} seconds`)),
@@ -23,26 +30,25 @@ const timeoutPromise = (seconds) =>
     ),
   );
 
-async function test(cmd, path, args = [], timeout = 120) {
+async function examplesTest(cmd: string, path: string, args: string[] = [], timeout = 120) {
   const file = basename(path);
   const cwd = dirname(path);
   const spawnedExample = spawn(cmd, [file, ...args], { stdio: "inherit", cwd });
   const testPromise = new Promise((res, rej) => {
     spawnedExample.on("close", (code, signal) => {
       if (code === 0) return res(true);
-      rej(`Example test exited with code ${code} by signal ${signal}`);
+      rej(`Example test "${file}" exited with code ${code} by signal ${signal}`);
     });
   });
   return Promise.race([timeoutPromise(timeout), testPromise]);
 }
 
-async function testAll(examples) {
-  const { apiKey } = await goth.start();
-  console.log(`Goth started with APIKEY:`, apiKey);
+async function testAll(examples: Example[]) {
+  await Promise.race([goth.start(), timeoutPromise(180)]);
   try {
     for (const example of examples) {
       console.log(`Starting test for example ${example.path}`);
-      await test(example.cmd, example.path, example.args);
+      await examplesTest(example.cmd, example.path, example.args);
     }
   } catch (error) {
     console.error(error);
