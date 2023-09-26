718d5152cbd65f8e51f0300049c8f8ecb0849d4c
Marcin Gordel <marcin.gordel@gmail.com>
2023-09-26 14:03:18 +0200

    test(examples): fixed cypress tests

--- tests/cypress/ui/docs-example-transfer-json.cy.ts
+++ tests/cypress/ui/docs-example-transfer-json.cy.ts
@@ -4,7 +4,7 @@ describe("Docs Examples Transfer JSON", () => {
     cy.get("#YAGNA_API_BASEPATH").clear().type(Cypress.env("YAGNA_API_BASEPATH"));
     cy.get("#SUBNET_TAG").clear().type(Cypress.env("YAGNA_SUBNET"));
     cy.get("#echo").click();
-    cy.get("#results").should("include.text", "TODO", { timeout: 60000 });
+    cy.get("#results").should("include.text", `"Hello World"`, { timeout: 60000 });
     cy.get("#logs").contains("computed by provider");
     cy.get("#logs").contains("Task Executor has shut down");
   });
--- tests/cypress/ui/docs-quickstart.cy.ts
+++ tests/cypress/ui/docs-quickstart.cy.ts
@@ -1,6 +1,6 @@
 describe("Docs Examples Quickstart", () => {
   it("should print hello world", () => {
-    cy.visit("/docs-example-quickstart");
+    cy.visit("/docs-quickstart");
     cy.get("#YAGNA_API_BASEPATH").clear().type(Cypress.env("YAGNA_API_BASEPATH"));
     cy.get("#SUBNET_TAG").clear().type(Cypress.env("YAGNA_SUBNET"));
     cy.get("#echo").click();
