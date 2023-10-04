4a8edb06f155e57b0c861ccf6c8c7edf5a6e3bcd
Phillip Jensen <phillip@golemgrid.com>
2023-10-04 11:40:46 +0200

    chore: continously fund the requestor

--- tests/e2e/start-requestor.sh
+++ tests/e2e/start-requestor.sh
@@ -5,33 +5,10 @@ get_funds_from_faucet() {
     yagna payment fund
 }
 
-# echo "Starting Yagna in the background to get funds from the faucet"
 echo "Starting Yagna"
-# yagna service run >/dev/null 2>&1 &
-yagna service run --api-allow-origin="*"
-# sleep 1
+yagna service run --api-allow-origin="*" & # start Yagna in the background
 
-# PID=$(pgrep -f "yagna service run")
-# echo "Yagna is running with PID: $PID"
-
-# sleep 4
-
-# echo "I will now try to get funds from the faucet"
-# FUNDING_STATUS="NO"
-
-# while [[ $FUNDING_STATUS == "NO" ]]; do
-#     if get_funds_from_faucet; then
-#         echo "Funds received from the faucet"
-#         FUNDING_STATUS="OK"
-#     else
-#         echo "Error receiving funds from the faucet. We're retrying..."
-#     fi
-# done
-
-# stop_yagna=$(kill -9 $PID)
-
-# echo "Stopped Yagna"
-# sleep 4
-
-# echo "Starting Yagna again to run it in the foreground"
-# yagna service run
+while true; do
+    get_funds_from_faucet
+    sleep 30
+done
