af7e2b2b59fe069c3f20b50806f1665d937bf80d
Marcin Gordel <marcin.gordel@gmail.com>
2023-10-18 12:16:09 +0200

    chore: decreased prices on providers config fixes in readme

--- tests/e2e/gftp.spec.ts
+++ tests/e2e/gftp.spec.ts
@@ -5,39 +5,43 @@ import fs from "fs";
 const logger = new LoggerMock(false);
 
 describe("GFTP transfers", function () {
-  it("should upload and download big files simultaneously", async () => {
-    const executor = await TaskExecutor.create({
-      package: "golem/alpine:latest",
-      logger,
-    });
+  it(
+    "should upload and download big files simultaneously",
+    async () => {
+      const executor = await TaskExecutor.create({
+        package: "golem/alpine:latest",
+        logger,
+      });
 
-    executor.beforeEach(async (ctx) => {
-      const sourcePath = fs.realpathSync(__dirname + "/../mock/fixtures/eiffel.blend");
-      await ctx.uploadFile(sourcePath, "/golem/work/eiffel.blend");
-    });
+      executor.beforeEach(async (ctx) => {
+        const sourcePath = fs.realpathSync(__dirname + "/../mock/fixtures/eiffel.blend");
+        await ctx.uploadFile(sourcePath, "/golem/work/eiffel.blend");
+      });
 
-    const data = [0, 1, 2, 3, 4, 5];
+      const data = [0, 1, 2, 3, 4, 5];
 
-    const results = executor.map(data, async (ctx, frame) => {
-      const result = await ctx
-        .beginBatch()
-        .run("ls -Alh /golem/work/eiffel.blend")
-        .downloadFile(`/golem/work/eiffel.blend`, `copy_${frame}.blend`)
-        .end()
-        .catch((error) => ctx.rejectResult(error.toString()));
-      return result ? `copy_${frame}.blend` : "";
-    });
+      const results = executor.map(data, async (ctx, frame) => {
+        const result = await ctx
+          .beginBatch()
+          .run("ls -Alh /golem/work/eiffel.blend")
+          .downloadFile(`/golem/work/eiffel.blend`, `copy_${frame}.blend`)
+          .end()
+          .catch((error) => ctx.rejectResult(error.toString()));
+        return result ? `copy_${frame}.blend` : "";
+      });
 
-    const expectedResults = data.map((d) => `copy_${d}.blend`);
+      const expectedResults = data.map((d) => `copy_${d}.blend`);
 
-    for await (const result of results) {
-      expect(expectedResults).toContain(result);
-    }
+      for await (const result of results) {
+        expect(expectedResults).toContain(result);
+      }
 
-    for (const file of expectedResults) {
-      expect(fs.existsSync(file)).toEqual(true);
-    }
+      for (const file of expectedResults) {
+        expect(fs.existsSync(file)).toEqual(true);
+      }
 
-    await executor.end();
-  });
+      await executor.end();
+    },
+    1000 * 240,
+  );
 });
