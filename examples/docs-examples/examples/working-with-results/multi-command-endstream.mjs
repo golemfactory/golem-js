9cd3aa943af3fe0ca7e784177067a5542e76973b
Seweryn Kras <seweryn.kras@golem.network>
2023-11-23 15:15:01 +0100

    chore: change multi command endstream  example to avoid terminating the executor mid-execution

--- examples/docs-examples/examples/working-with-results/multi-command-endstream.mjs
+++ examples/docs-examples/examples/working-with-results/multi-command-endstream.mjs
@@ -6,7 +6,7 @@ import { TaskExecutor } from "@golem-sdk/golem-js";
     yagnaOptions: { apiKey: "try_golem" },
   });
 
-  const result = await executor.run(async (ctx) => {
+  await executor.run(async (ctx) => {
     const res = await ctx
       .beginBatch()
       .uploadFile("./worker.mjs", "/golem/input/worker.mjs")
@@ -17,6 +17,7 @@ import { TaskExecutor } from "@golem-sdk/golem-js";
 
     res.on("data", (result) => console.log(result));
     res.on("error", (error) => console.error(error));
-    res.on("close", () => executor.shutdown());
+    return new Promise((resolve) => res.on("close", resolve));
   });
+  await executor.shutdown();
 })();
