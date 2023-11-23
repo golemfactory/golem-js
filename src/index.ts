582b9507b5ac648125f61b34cc45181e16075926
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2023-11-23 10:25:07 +0100

    feature: expose Golem services from the SDK

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,19 @@ export {
   WebSocketBrowserStorageProvider,
   WebSocketStorageProviderOptions,
 } from "./storage";
-export { ActivityStateEnum, Result } from "./activity";
+export { ActivityStateEnum, Result, Activity } from "./activity";
 export { AgreementCandidate, AgreementSelectors } from "./agreement";
 export { ProposalFilters, ProposalFilter, MarketHelpers } from "./market";
 export { Package, PackageOptions } from "./package";
 export { PaymentFilters } from "./payment";
 export { Events, BaseEvent, EventType } from "./events";
 export { Logger, LogLevel, jsonLogger, nullLogger, consoleLogger, pinoLogger, defaultLogger } from "./utils";
-export { Yagna } from "./utils/yagna/yagna";
+export { Yagna, YagnaApi } from "./utils/yagna/yagna";
 export { Job, JobStorage, JobState } from "./job";
 export { GolemNetwork, GolemNetworkConfig } from "./golem_network";
-export { Worker, WorkContext } from "./task";
+
+// v1.0.0
+export { AgreementPoolService } from "./agreement/service";
+export { MarketService } from "./market/service";
+export { PaymentService } from "./payment/service";
+export { WorkContext, Worker } from "./task";
