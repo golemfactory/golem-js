6d21225b34ffae3011d95c75583c59bb5c13c3e7
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2023-11-29 14:42:10 +0100

    refactor: switch back to older logger configuration

--- src/utils/logger/defaultLogger.ts
+++ src/utils/logger/defaultLogger.ts
@@ -8,8 +8,6 @@ export function defaultLogger(filename?: string) {
         destination: filename,
         colorize: !filename,
         ignore: "pid,hostname",
-        /** {@see https://github.com/pinojs/pino-pretty#usage-with-jest} */
-        sync: true,
       },
     },
   });
