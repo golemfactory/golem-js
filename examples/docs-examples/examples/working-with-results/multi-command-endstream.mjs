b79da87046d96103a174f2c9a44bbc7f4f2f7be8
Marcin Gordel <marcin.gordel@gmail.com>
2023-12-18 10:14:56 +0100

    test: fixed test for stream example

--- examples/docs-examples/examples/working-with-results/multi-command-endstream.mjs
+++ examples/docs-examples/examples/working-with-results/multi-command-endstream.mjs
@@ -3,7 +3,7 @@ import { TaskExecutor } from "@golem-sdk/golem-js";
 (async () => {
   const executor = await TaskExecutor.create({
     package: "529f7fdaf1cf46ce3126eb6bbcd3b213c314fe8fe884914f5d1106d4",
-    // yagnaOptions: { apiKey: "try_golem" },
+    yagnaOptions: { apiKey: "try_golem" },
   });
 
   try {
