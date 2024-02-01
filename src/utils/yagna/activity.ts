336ccaecfb0501a8c4383e80eeb755c849b2a61a
Seweryn Kras <seweryn.kras@golem.network>
2024-02-01 15:55:57 +0100

    feat(yagna-api): add the endpoint to fetch agreement id from activity id

---
+++ src/utils/yagna/activity.ts
@@ -0,0 +1,17 @@
+import { RequestorStateApi } from "ya-ts-client/dist/ya-activity";
+import { GolemPlatformError } from "../../error/golem-error";
+import { Agreement } from "ya-ts-client/dist/ya-market";
+
+export class RequestorApi extends RequestorStateApi {
+  async getActivityAgreementId(activityId: string): Promise<Agreement["agreementId"]> {
+    const res = await fetch(this.basePath + "/activity/" + activityId + "/agreement", {
+      headers: { authorization: `Bearer ${this.configuration?.apiKey}` },
+    });
+
+    if (!res.ok) {
+      throw new GolemPlatformError(`Failed to get activity agreement: ${res.statusText}`);
+    }
+
+    return await res.json();
+  }
+}
