19f2e0378a55c88bc2bfc6870e5e0310f1441795
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2024-02-14 15:01:34 +0100

    docs: updated blender example to provide more logs

--- examples/blender/blender.ts
+++ examples/blender/blender.ts
@@ -1,6 +1,7 @@
 import { TaskExecutor } from "@golem-sdk/golem-js";
 import { program } from "commander";
 import { fileURLToPath } from "url";
+
 const DIR_NAME = fileURLToPath(new URL(".", import.meta.url));
 
 const blenderParams = (frame) => ({
@@ -21,6 +22,7 @@ const blenderParams = (frame) => ({
   WORK_DIR: "/golem/work",
   OUTPUT_DIR: "/golem/output",
 });
+
 async function main(subnetTag: string, driver?: string, network?: string, maxParallelTasks?: number) {
   const executor = await TaskExecutor.create({
     subnetTag,
@@ -31,21 +33,32 @@ async function main(subnetTag: string, driver?: string, network?: string, maxPar
 
   try {
     executor.onActivityReady(async (ctx) => {
+      console.log("Uploading the scene to the provider %s", ctx.provider.name);
       await ctx.uploadFile(`${DIR_NAME}/cubes.blend`, "/golem/resource/scene.blend");
+      console.log("Upload of the scene to the provider %s finished", ctx.provider.name);
     });
 
-    const futureResults = [0, 10, 20, 30, 40, 50].map((frame) =>
+    const futureResults = [0, 10, 20, 30, 40, 50].map(async (frame) =>
       executor.run(async (ctx) => {
+        console.log("Started rendering of frame %d on provider %s", frame, ctx.provider.name);
+
         const result = await ctx
           .beginBatch()
           .uploadJson(blenderParams(frame), "/golem/work/params.json")
           .run("/golem/entrypoints/run-blender.sh")
           .downloadFile(`/golem/output/out${frame?.toString().padStart(4, "0")}.png`, `${DIR_NAME}/output_${frame}.png`)
           .end();
+
+        console.log("Finished rendering of frame %d on provider %s", frame, ctx.provider.name);
+
         return result?.length ? `output_${frame}.png` : "";
       }),
     );
+
+    console.log("Scheduling all tasks");
     const results = await Promise.all(futureResults);
+    console.log("Completed all tasks");
+
     results.forEach((result) => console.log(result));
   } catch (error) {
     console.error("Computation failed:", error);
