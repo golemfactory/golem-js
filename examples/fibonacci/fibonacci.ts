2f3f0b102fe95c2843fc3ede6139dea4950385c3
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2024-02-15 09:46:27 +0100

    docs: migrated fibonacci example to TS and provided image building scripts

--- examples/fibonacci/fibonacci.ts
+++ examples/fibonacci/fibonacci.ts
@@ -1,24 +1,21 @@
 import { TaskExecutor } from "@golem-sdk/golem-js";
 import { program } from "commander";
 
-async function main(fiboN = 1, tasksCount = 1, subnetTag, driver, network) {
+async function main(subnetTag: string, driver: string, network: string, tasksCount = 1, fiboN = 1) {
   const executor = await TaskExecutor.create({
     package: "golem/js-fibonacci:latest",
     subnetTag,
     payment: { driver, network },
   });
 
-  const runningTasks = [];
+  const runningTasks: Promise<string | undefined>[] = [];
   for (let i = 0; i < tasksCount; i++) {
     runningTasks.push(
-      executor
-        .run(async (ctx) => {
-          const result = await ctx.run("/usr/local/bin/node", ["/golem/work/fibo.js", fiboN.toString()]);
-          console.log(result.stdout);
-        })
-        .catch((error) => {
-          console.error("Task execution error:", error);
-        }),
+      executor.run(async (ctx) => {
+        const result = await ctx.run("/usr/local/bin/node", ["/golem/work/fibo.js", fiboN.toString()]);
+        console.log(result.stdout);
+        return result.stdout?.toString().trim();
+      }),
     );
   }
 
@@ -35,6 +32,11 @@ program
   .option("--subnet-tag <subnet>", "set subnet name, for example 'public'")
   .option("--payment-driver, --driver <driver>", "payment driver name, for example 'erc20'")
   .option("--payment-network, --network <network>", "network name, for example 'goerli'");
+
 program.parse();
+
 const options = program.opts();
-main(options.fibonacciNumber, options.tasksCount, options.subnetTag, options.driver, options.network);
+
+main(options.subnetTag, options.driver, options.network, options.tasksCount, options.fibonacciNumber).catch((err) =>
+  console.error("Error in main", err),
+);
