f1ea3b6719747fdd15bc6530c75e6907320cccf1
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2024-04-10 17:00:36 +0200

    chore: documented the GolemNetwork and provided basic events

--- src/golem-network.ts
+++ src/golem-network.ts
@@ -5,6 +5,7 @@ import { MarketModule, MarketModuleImpl } from "./market/market.module";
 import { PaymentModule, PaymentModuleImpl } from "./payment/payment.module";
 import { ActivityModule, ActivityModuleImpl } from "./activity/activity.module";
 import { NetworkModule, NetworkModuleImpl } from "./network/network.module";
+import { EventEmitter } from "eventemitter3";
 
 export interface GolemNetworkOptions {
   logger?: Logger;
@@ -18,7 +19,26 @@ export interface GolemNetworkOptions {
   dataTransferProtocol?: "gftp" | "ws" | StorageProvider;
 }
 
+export interface GolemNetworkEvents {
+  /** Fires when all startup operations related to GN are completed */
+  connected: () => void;
+
+  /** Fires when an error will be encountered */
+  error: (err: Error) => void;
+
+  /** Fires when all shutdown operations related to GN are completed */
+  disconnected: () => void;
+}
+
+/**
+ * General purpose and high-level API for the Golem Network
+ *
+ * This class is the main entry-point for developers that would like to build on Golem Network
+ * using `@golem-sdk/golem-js`. It is supposed to provide an easy access API for use 80% of use cases.
+ */
 export class GolemNetwork {
+  public readonly events = new EventEmitter<GolemNetworkEvents>();
+
   private readonly yagna: YagnaApi;
   public readonly market: MarketModule;
   public readonly payment: PaymentModule;
@@ -26,22 +46,53 @@ export class GolemNetwork {
   public readonly network: NetworkModule;
 
   constructor(public readonly options: GolemNetworkOptions) {
-    this.yagna = new YagnaApi();
-    this.market = new MarketModuleImpl(this.yagna);
-    this.payment = new PaymentModuleImpl();
-    this.activity = new ActivityModuleImpl();
-    this.network = new NetworkModuleImpl();
+    try {
+      this.yagna = new YagnaApi({
+        logger: this.options.logger,
+        apiKey: this.options.api.key,
+        basePath: this.options.api.url,
+      });
+
+      this.market = new MarketModuleImpl(this.yagna);
+      this.payment = new PaymentModuleImpl();
+      this.activity = new ActivityModuleImpl();
+      this.network = new NetworkModuleImpl();
+    } catch (err) {
+      this.events.emit("error", err);
+      throw err;
+    }
   }
 
+  /**
+   * "Connects" to the network by initializing the underlying components required to perform operations on Golem Network
+   *
+   * @return Resolves when all initialization steps are completed
+   */
   async connect() {
-    // todo
+    try {
+      await this.yagna.connect();
+      this.events.emit("connected");
+    } catch (err) {
+      this.events.emit("error", err);
+      throw err;
+    }
   }
 
+  /**
+   * "Disconnects" from the Golem Network
+   *
+   * @return Resolves when all shutdown steps are completed
+   */
   async disconnect() {
-    // todo
+    this.events.emit("disconnected");
   }
 
-  createBuilder(): GolemDeploymentBuilder {
+  /**
+   * Creates a new instance of deployment builder that will be bound to this GolemNetwork instance
+   *
+   * @return The new instance of the builder
+   */
+  creteDeploymentBuilder(): GolemDeploymentBuilder {
     return new GolemDeploymentBuilder(this);
   }
 }
