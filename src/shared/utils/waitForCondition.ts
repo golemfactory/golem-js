aebb8ee4959334048fde7d4f773bfdfd79089cef
Grzegorz Godlewski <ggodlewski@sorcerystudio.com>
2024-04-17 21:06:24 +0200

    chore: applied remark from CR

--- src/shared/utils/waitForCondition.ts
+++ src/shared/utils/waitForCondition.ts
@@ -2,18 +2,21 @@ import { clearInterval } from "node:timers";
 import { GolemTimeoutError } from "../error/golem-error";
 
 /**
- * Utility function that helps to block the execution until a condition is met (check returns true) or the timeout happens
+ * Utility function that helps to block the execution until a condition is met (check returns true) or the timeout happens.
  *
- * @param check The function checking if the condition is met
- * @param opts Options controlling the timeout and check interval in seconds
+ * @param {function} check - The function checking if the condition is met.
+ * @param {Object} [opts] - Options controlling the timeout and check interval in seconds.
+ * @param {number} [opts.timeoutSeconds=15] - The timeout value in seconds. Default is 15 seconds.
+ * @param {number} [opts.intervalSeconds=1] - The interval between condition checks in seconds. Default is 1 second.
  *
- * @return Resolves when the condition is met or rejects with a timeout error if it wasn't met on time
+ * @return {Promise<void>} - Resolves when the condition is met or rejects with a timeout error if it wasn't met on time.
  */
 export function waitForCondition(
   check: () => boolean,
   opts = { timeoutSeconds: 15, intervalSeconds: 1 },
 ): Promise<void> {
   let verifyInterval: NodeJS.Timeout | undefined;
+  let waitTimeout: NodeJS.Timeout | undefined;
 
   const verify = new Promise<void>((resolve) => {
     verifyInterval = setInterval(() => {
@@ -24,11 +27,14 @@ export function waitForCondition(
     }, opts.intervalSeconds * 1000);
   });
 
-  const timeout = new Promise<void>((_, reject) => {
-    setTimeout(() => {
+  const wait = new Promise<void>((_, reject) => {
+    waitTimeout = setTimeout(() => {
       reject(new GolemTimeoutError(`Condition was not met within ${opts.timeoutSeconds}s`));
     }, opts.timeoutSeconds * 1000);
   });
 
-  return Promise.race([verify, timeout]).finally(() => clearInterval(verifyInterval));
+  return Promise.race([verify, wait]).finally(() => {
+    clearInterval(verifyInterval);
+    clearTimeout(waitTimeout);
+  });
 }
