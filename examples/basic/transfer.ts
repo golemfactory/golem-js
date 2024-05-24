2f54a26e97922b6600ebbf5bf1fc9f63f3ec1de3
Marcin Gordel <marcin.gordel@gmail.com>
2024-05-24 14:29:08 +0200

    docs: added basic example of file transfer

---
+++ examples/basic/transfer.ts
@@ -0,0 +1,67 @@
+import { MarketOrderSpec, GolemNetwork } from "@golem-sdk/golem-js";
+import { pinoPrettyLogger } from "@golem-sdk/pino-logger";
+import { appendFile, readFile, unlink } from "fs/promises";
+
+const order: MarketOrderSpec = {
+  demand: {
+    workload: { imageTag: "golem/alpine:latest" },
+  },
+  market: {
+    maxAgreements: 2,
+    rentHours: 0.5,
+    pricing: {
+      model: "linear",
+      maxStartPrice: 0.5,
+      maxCpuPerHourPrice: 1.0,
+      maxEnvPerHourPrice: 0.5,
+    },
+  },
+};
+
+(async () => {
+  const glm = new GolemNetwork({
+    logger: pinoPrettyLogger({
+      level: "info",
+    }),
+  });
+
+  try {
+    await glm.connect();
+    const pool = await glm.manyOf({
+      concurrency: 2,
+      order,
+    });
+    const lease1 = await pool.acquire();
+    const lease2 = await pool.acquire();
+
+    const exe1 = await lease1.getExeUnit();
+    const exe2 = await lease2.getExeUnit();
+
+    await exe1
+      .beginBatch()
+      .run(`echo "Message from provider ${exe1.provider.name}. Hello 😻" >> /golem/work/message.txt`)
+      .downloadFile("/golem/work/message.txt", "./message.txt")
+      .end();
+
+    await appendFile("./message.txt", "Message from requestor. Hello 🤠\n");
+
+    await exe2
+      .beginBatch()
+      .uploadFile("./message.txt", "/golem/work/message.txt")
+      .run(`echo "Message from provider ${exe2.provider.name}. Hello 👻" >> /golem/work/message.txt`)
+      .downloadFile("/golem/work/message.txt", "./results.txt")
+      .end();
+
+    console.log("File content: ");
+    console.log(await readFile("./results.txt", { encoding: "utf-8" }));
+
+    await lease1.finalize();
+    await lease2.finalize();
+  } catch (err) {
+    console.error("Failed to run the example", err);
+  } finally {
+    await glm.disconnect();
+    await unlink("./message.txt");
+    await unlink("./results.txt");
+  }
+})().catch(console.error);
