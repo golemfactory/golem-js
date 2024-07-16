b4e317849602cca3f31d305c8456cc8947de0ea0
Seweryn Kras <seweryn.kras@golem.network>
2024-07-16 17:42:28 +0200

    docs: remove filter in many-of example

--- examples/rental-model/basic/many-of.ts
+++ examples/rental-model/basic/many-of.ts
@@ -17,9 +17,6 @@ const order: MarketOrderSpec = {
       maxCpuPerHourPrice: 1.0,
       maxEnvPerHourPrice: 0.5,
     },
-    offerProposalFilter: (offer) => {
-      return offer.provider.name === "q53.h";
-    },
   },
 };
 
