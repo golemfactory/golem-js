f8c629d462b56a207b4515e49d1ebe970d22868d
Marcin Gordel <marcin.gordel@gmail.com>
2024-08-29 12:08:37 +0200

    fix(remote-process): expose RemoteProcess as public API object

--- src/activity/exe-unit/index.ts
+++ src/activity/exe-unit/index.ts
@@ -1,4 +1,5 @@
 export { ExeUnit, LifecycleFunction, ExeUnitOptions } from "./exe-unit";
 export { Batch } from "./batch";
+export { RemoteProcess } from "./process";
 export { GolemWorkError, WorkErrorCode } from "./error";
 export { TcpProxy } from "../../network/tcp-proxy";
